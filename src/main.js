6fc5e800e3475506a8d89167aacc32c5398f13df
lkszkr <147940121+lkszkr@users.noreply.github.com>
2026-02-04 23:31:52 +0100

    implement updateAside, add constructor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import './styles/styles.css';
-import * as API from './modules/api.js'
 import View from './modules/view.js';
 
-const a = new View();
-a.handleInput();
+new View();
--- src/modules/view.js
+++ src/modules/view.js
@@ -3,7 +3,23 @@ import { resolveWeatherCode } from './utils.js';
 
 class View {
 
-    handleInput() {
+    #locationDataDefault = {
+        lat: 52.22977,
+        lng: 21.01178,
+        country: 'Poland',
+        name: 'Warsaw',
+    };
+    #currentLocationData = this.#locationDataDefault;
+    #currentWehaterData = null;
+
+    constructor() {
+        (async () => await this.#updateContent(this.#locationDataDefault))();
+        document.querySelector('.nav-btn').classList.add('btn-active');
+        this.#setupButtons();
+        this.#handleInput();
+    }
+
+    #handleInput() {
         const ul = document.querySelector('.suggestions');
         const input = document.querySelector('#search');
         let timer;
@@ -35,11 +51,12 @@ class View {
         });
     }
 
-    async updateContent(locationData) {
-        const data = await api.fetchWeather(locationData.lat, locationData.lng);
-        this.#updateHeader(locationData.name, locationData.country);
-        this.#updateMain(data.current, 'Current');
-        // this.#updateAside(data.hourly, data.daily);
+    async #updateContent(locData) {
+        const data = await api.fetchWeather(locData.lat, locData.lng);
+        this.#currentWehaterData = data;
+        this.#updateHeader(locData.name, locData.country);
+        this.#updateMain(data.current, 'current');
+        this.#updateAside(data.hourly.slice(0, 24));
     }
 
     #updateHeader(city, country) {
@@ -48,13 +65,18 @@ class View {
     }
 
     async #updateMain(data, dataTitle) {
-
         const weatherCodeData = resolveWeatherCode(data.weatherCode, data.isNight);
         this.#updateSvg('.weather-icon', weatherCodeData.svg);
         this.#updateSpan('.weather-desc', weatherCodeData.desc);
 
         this.#updateSpan('.title', dataTitle);
-        this.#updateSpan('.current-time', data.time.toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric' }));
+
+        if (dataTitle === 'days') {
+            this.#updateSpan('.current-time', data.time.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' }));
+        } else {
+            this.#updateSpan('.current-time', data.time.toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric' }));
+        }
+
         this.#updateSpan('.temperature', data.temperature);
         this.#updateSpan('.precipitationProbability', data.precipitationProbability);
         this.#updateSpan('.precipitation', data.precipitation);
@@ -65,11 +87,67 @@ class View {
         this.#updateSpan('.europeanAqi', data.europeanAqi);
         this.#updateSpan('.sunrise', data.sunrise.toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric' }));
         this.#updateSpan('.sunset', data.sunset.toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric' }));
-
     }
 
-    #updateAside() {
+    #updateAside(data, dataTitle) {
+        const ul = document.querySelector('.forecast-list');
+        ul.textContent = '';
 
+        data.forEach((element) => {
+            const template = document.querySelector('template');
+            const tempContent = template.content.cloneNode(true);
+            const forecastTile = tempContent.querySelector('.forecast-tile');
+
+            const weatherCodeData = resolveWeatherCode(element.weatherCode, element.isNight);
+            this.#updateSvg('.forecast-icon', weatherCodeData.svg, forecastTile);
+            this.#updateSpan('.forecast-desc', weatherCodeData.desc, forecastTile);
+
+            if (dataTitle === 'days') {
+                this.#updateSpan('.forecast-time', element.time.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' }), forecastTile);
+            } else {
+                this.#updateSpan('.forecast-time', element.time.toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric' }), forecastTile);
+            }
+
+            this.#updateSpan('.forecast-temperature', element.temperature, forecastTile);
+            this.#updateSpan('.forecast-windSpeed', element.windSpeed, forecastTile);
+            this.#updateSpan('.forecast-precipitation', element.precipitation, forecastTile);
+
+            forecastTile.addEventListener('click', () => {
+                if (!dataTitle) dataTitle = 'today';
+                this.#updateMain(element, dataTitle);
+            });
+
+            ul.appendChild(forecastTile);
+        });
+    }
+
+    #setupButtons() {
+        const btns = document.querySelectorAll('.nav-btn');
+        btns.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                btns.forEach((b) => b.classList.remove('btn-active'))
+                btn.classList.add('btn-active');
+                let data;
+                switch (btn.value) {
+                    case 'today':
+                        data = this.#currentWehaterData.hourly.slice(0, 24);
+                        break;
+                    case 'tomorrow':
+                        data = this.#currentWehaterData.hourly.slice(24);
+                        break;
+                    case 'days':
+                        data = this.#currentWehaterData.daily;
+                        break;
+                }
+                this.#updateAside(data, btn.value);
+            });
+        });
+        const currentBtn = document.querySelector('.current-btn');
+        currentBtn.addEventListener('click', () => {
+            this.#updateContent(this.#currentLocationData);
+            btns.forEach((b) => b.classList.remove('btn-active'))
+            document.querySelector('.nav-btn').classList.add('btn-active');
+        });
     }
 
     #setupValidation() {
@@ -106,7 +184,8 @@ class View {
             li.setAttribute('data-name', element.name);
             li.setAttribute('data-country', element.country);
             li.addEventListener('mousedown', (e) => {
-                this.updateContent(element);
+                this.#currentLocationData = element;
+                this.#updateContent(element);
                 ul.textContent = '';
                 input.value = '';
                 ul.classList.remove('active');
@@ -115,16 +194,15 @@ class View {
         });
     }
 
-    #updateSpan(selector, value) {
-        const span = document.querySelector(selector);
+    #updateSpan(selector, value, container = document,) {
+        const span = container.querySelector(selector);
         if (span) span.textContent = value + " ";
     }
 
-    #updateSvg(selector, value) {
-        const svg = document.querySelector(selector);
+    #updateSvg(selector, value, container = document) {
+        const svg = container.querySelector(selector);
         if (svg) svg.innerHTML = `<img src="${value}" alt="weather">`;
     }
 }
-
 
 export default View;
